2824f29f1c2852456530808edd709ffdfc1d97d9
Prajwal V <prajwalv227@gmail.com>
2026-09-08 11:59:14 +0530

    Add files via upload

--- sumof4.java
+++ sumof4.java
@@ -1,16 +1,13 @@
- public class sumof4 {
+public class sumof4 {
     public static void main(String[] args) {
         int num = 1234;
+        int sum = 0;
 
-        int a = num / 1000;
-        int b = (num / 100) % 10;
-        int c = (num / 10) % 10;
-        int d = num % 10;
-
-        int sum = a + b + c + d;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
 
         System.out.println("Sum of digits = " + sum);
-    
-
-}
+    }
 }
