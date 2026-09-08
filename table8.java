3e60a68cf5093dd28973892493a9f9d5ebe2bbce
Prajwal V <prajwalv227@gmail.com>
2026-09-08 11:25:43 +0530

    Add files via upload

---
+++ table8.java
@@ -0,0 +1,10 @@
+public class table8 {
+    public static void main(String[] args) {
+
+        int num = 8;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num +"*" + i + "=" + (num * i));
+        }
+    }
+}
